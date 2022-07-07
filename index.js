605172823cdd8053cf270d9dea2909b36e4a308b
Justin Pham <justin.tk.pham@gmail.com>
2022-07-07 19:05:18 +1000

    Add image on click functional

--- index.js
+++ index.js
@@ -4,12 +4,19 @@
 // create them with factories.
 
 const BLANK = false;
+let currentPlayer = null;
 
 const game = (() => {
 
+    const pvp = document.querySelector('#pvp');
+    pvp.addEventListener('click', () => startPVPGame());
+
+    function startPVPGame() {
+        currentPlayer = Player('x', 'images/x.png');
+    }
+
 
     return {
-
     };
 })();
 
@@ -18,17 +25,40 @@ const board = (() => {
                 [BLANK, BLANK, BLANK],
                 [BLANK, BLANK, BLANK]];
 
-    
+    const cells = document.querySelectorAll('.cell');
+    cells.forEach(element => {
+        element.addEventListener('click', event => placeMarker(event.target.dataset.index));
+    });
+
+    function placeMarker(index) {
+        indexSplit = index.split(',');
+        const x = indexSplit[0];
+        const y = indexSplit[1];
+
+        if (board[x][y] === BLANK) {
+            board[x][y] = currentPlayer.getSymbol();
+            const cell = document.querySelector(`div[data-index="${index}"]`);
+            cell.appendChild(currentPlayer.getImg());
+        }
+    }
+
     return {
 
     };
 })();
 
-const Player = () => {
+const Player = (symbol, imageURL) => {
+    const getSymbol = () => symbol;
+
+    const img = document.createElement('img');
+    img.setAttribute('src', imageURL);
+
+    const getImg = () => img;
 
 
     return {
-
+        getSymbol,
+        getImg
     };
 };
 
